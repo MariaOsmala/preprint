a053ca02cfced9e9ebee958dab358c70a8697f25
Marijn van Vliet <w.m.vanvliet@gmail.com>
2021-01-13 15:25:15 +0200

    Fix create_dirs.sh

--- create_dirs.sh
+++ create_dirs.sh
@@ -5,27 +5,26 @@ path_to_dir=../preprint #specify
 cd $path_to_dir
 
 
-mkdir $path_to_dir/softwares/
-mkdir $path_to_dir/softwares/R_packages/
-mkdir $path_to_dir/softwares/R_packages/R-3.4.3/
-mkdir $path_to_dir/softwares/utils/
-mkdir $path_to_dir/softwares/bin/
-mkdir $path_to_dir/softwares/genome_indexes/.../genome # put BOWTIE2 indexes here
+mkdir $path_to_dir/softwares
+mkdir $path_to_dir/softwares/R_packages
+mkdir $path_to_dir/softwares/R_packages/R-3.4.3
+mkdir $path_to_dir/softwares/utils
+mkdir $path_to_dir/softwares/bin
+mkdir -p $path_to_dir/softwares/genome_indexes/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome # put BOWTIE2 indexes here
 
 mkdir $path_to_dir/Data
-cd $path_to_dir/Data
-mkdir K562
-mkdir GM12878
-mkdir blacklists
-mkdir GENCODE_TSS
-mkdir K562/ChromHMM
-mkdir GM12878/ChromHMM
-mkdir K562/TFs #narrowPeak files
-mkdir GM12878/TFs #narrowPeak files
-mkdir K562/raw_data #fastq files
-mkdir GM12878/raw_data #fastq files
-mkdir K562/bam_replicates #DNase-seq and MNase-seq data as .bam and .bam.bai here
-mkdir GM12878/bam_replicates #DNase-seq and MNase-seq data as .bam and .bam.bai here
+mkdir $path_to_dir/Data/K562
+mkdir $path_to_dir/Data/GM12878
+mkdir $path_to_dir/Data/blacklists
+mkdir $path_to_dir/Data/GENCODE_TSS
+mkdir $path_to_dir/Data/K562/ChromHMM
+mkdir $path_to_dir/Data/GM12878/ChromHMM
+mkdir $path_to_dir/Data/K562/TFs #narrowPeak files
+mkdir $path_to_dir/Data/GM12878/TFs #narrowPeak files
+mkdir $path_to_dir/Data/K562/raw_data #fastq files
+mkdir $path_to_dir/Data/GM12878/raw_data #fastq files
+mkdir $path_to_dir/Data/K562/bam_replicates #DNase-seq and MNase-seq data as .bam and .bam.bai here
+mkdir $path_to_dir/Data/GM12878/bam_replicates #DNase-seq and MNase-seq data as .bam and .bam.bai here
 
 #put p300 peak files to these folders
 #$path_to_dir/Data/K562/TFs/wgEncodeAwgTfbsSydhK562P300IggrabUniPk.narrowPeak.gz
@@ -35,29 +34,29 @@ mkdir GM12878/bam_replicates #DNase-seq and MNase-seq data as .bam and .bam.bai 
 #$path_to_dir/Data/K562/raw_data/wgEncodeOpenChromDnaseK562PkV2.narrowPeak.gz
 #$path_to_dir/Data/GM12878/raw_data/wgEncodeOpenChromDnaseGm12878Pk.narrowPeak.gz
 
-mkdir K562/bam_combined
-mkdir GM12878/bam_combined
-mkdir K562/phantompeakqualtools
-mkdir GM12878/phantompeakqualtools
+mkdir $path_to_dir/Data/K562/bam_combined
+mkdir $path_to_dir/Data/GM12878/bam_combined
+mkdir $path_to_dir/Data/K562/phantompeakqualtools
+mkdir $path_to_dir/Data/GM12878/phantompeakqualtools
 
-mkdir $path_to_dir/Data/K562/bed_combined/
-mkdir $path_to_dir/Data/K562/bed_shifted/
-mkdir $path_to_dir/Data/K562/bam_shifted/
+mkdir $path_to_dir/Data/K562/bed_combined
+mkdir $path_to_dir/Data/K562/bed_shifted
+mkdir $path_to_dir/Data/K562/bam_shifted
 
-mkdir $path_to_dir/Data/GM12878/bed_combined/
-mkdir $path_to_dir/Data/GM12878/bed_shifted/
-mkdir $path_to_dir/Data/GM12878/bam_shifted/
+mkdir $path_to_dir/Data/GM12878/bed_combined
+mkdir $path_to_dir/Data/GM12878/bed_shifted
+mkdir $path_to_dir/Data/GM12878/bam_shifted
 
-mkdir $path_to_dir/Data/K562/bed_shifted_RFECS/
-mkdir $path_to_dir/Data/GM12878/bed_shifted_RFECS/
+mkdir $path_to_dir/Data/K562/bed_shifted_RFECS
+mkdir $path_to_dir/Data/GM12878/bed_shifted_RFECS
 
-mkdir $path_to_dir/Data/K562/coverage_bigWig/
-mkdir $path_to_dir/Data/GM12878/coverage_bigWig/
+mkdir $path_to_dir/Data/K562/coverage_bigWig
+mkdir $path_to_dir/Data/GM12878/coverage_bigWig
 
 
-mkdir $path_to_dir/figures/
-mkdir $path_to_dir/Data/K562/data_R/
-mkdir $path_to_dir/Data/GM12878/data_R/
+mkdir $path_to_dir/figures
+mkdir $path_to_dir/Data/K562/data_R
+mkdir $path_to_dir/Data/GM12878/data_R
 
 mkdir $path_to_dir/Data/intervals_bed_100
 #intervals_data_folder, data in 100 bp intervals, whole genome
@@ -65,10 +64,10 @@ mkdir $path_to_dir/Data/K562/intervals_data_100
 mkdir $path_to_dir/Data/GM12878/intervals_data_100
 
 
-mkdir $path_to_dir/results/
-mkdir $path_to_dir/results/model_promoters_and_random_combined/
-mkdir $path_to_dir/results/model_promoters_and_random_combined/K562/
-mkdir $path_to_dir/results/model_promoters_and_random_combined/GM12878/
+mkdir $path_to_dir/results
+mkdir $path_to_dir/results/model_promoters_and_random_combined
+mkdir $path_to_dir/results/model_promoters_and_random_combined/K562
+mkdir $path_to_dir/results/model_promoters_and_random_combined/GM12878
 
 mkdir $path_to_dir/results/model_promoters_and_random_combined/K562/ML
 mkdir $path_to_dir/results/model_promoters_and_random_combined/K562/ML/5-fold_CV_1
@@ -86,14 +85,14 @@ mkdir $path_to_dir/results/model_promoters_and_random_combined/K562/Bayes_estima
 mkdir $path_to_dir/results/model_promoters_and_random_combined/GM12878/ML
 mkdir $path_to_dir/results/model_promoters_and_random_combined/GM12878/Bayes_estimated_priors
 
-mkdir $path_to_dir/results/RFECS_combined/
+mkdir $path_to_dir/results/RFECS_combined
 mkdir $path_to_dir/results/RFECS_combined/K562
-mkdir $path_to_dir/results/RFECS_combined/K562/training/
-mkdir $path_to_dir/results/RFECS_combined/K562/trained_forests/
-mkdir $path_to_dir/results/RFECS_combined/K562/whole_genome_predictions/
-mkdir $path_to_dir/results/RFECS_combined/K562/bedfiles/
+mkdir $path_to_dir/results/RFECS_combined/K562/training
+mkdir $path_to_dir/results/RFECS_combined/K562/trained_forests
+mkdir $path_to_dir/results/RFECS_combined/K562/whole_genome_predictions
+mkdir $path_to_dir/results/RFECS_combined/K562/bedfiles
 
-mkdir $path_to_dir/results/RFECS_combined/GM12878/
+mkdir $path_to_dir/results/RFECS_combined/GM12878
 
 mkdir $path_to_dir/results/RFECS_combined/GM12878/whole_genome_predictions
 mkdir $path_to_dir/results/RFECS_combined/GM12878/bedfiles
