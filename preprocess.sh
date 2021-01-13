5addf72408ea2ae92fa6f15dec37787d9b2b0cdc
Marijn van Vliet <w.m.vanvliet@gmail.com>
2021-01-13 16:53:47 +0200

    fix uppercase/lowercase variable name conventions

--- preprocess.sh
+++ preprocess.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
 
 PREPRINT_DIR=`pwd`
-DATA_DIR=$PREPRINT_DIR/Data
+
+data_dir=$PREPRINT_DIR/Data
 
 # Step 1: Align the reads
-BOWTIE_INDEXES=$PREPRINT_DIR/softwares/genome_indexes/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome
-BOWTIE_PARAMETERS="-x $BOWTIE_INDEXES"
+bowtie_indexes=$PREPRINT_DIR/softwares/genome_indexes/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome
+bowtie_parameters="-x $bowtie_indexes"
 
-CELL_LINE=K562
-RAW_DATA_DIR=$PREPRINT_DIR/Data/$CELL_LINE/raw_data
-BAM_OUTPUT_DIR=$PREPRINT_DIR/Data/$CELL_LINE/bam_replicates
+cell_line=K562
+raw_data_dir=$data_dir/$cell_line/raw_data
+bam_output_dir=$data_dir/$cell_line/bam_replicates
 
-RAW_FILE=wgEncodeBroadHistoneK562CtcfStdRawDataRep1.fastq.gz
-BAM_OUTPUT_FILE=${RAW_FILE%".fastq"*}".bam"
+raw_file=wgEncodeBroadHistoneK562CtcfStdRawDataRep1.fastq.gz
+bam_output_file=${raw_file%".fastq"*}".bam"
 
-echo $RAW_FILE
-echo $BAM_OUTPUT_FILE
-
-bowtie2 $BOWTIE_PARAMETERS -U $RAW_DATA_DIR/$RAW_FILE \
+bowtie2 $bowtie_parameters -U $raw_data_dir/$raw_file \
 	| samtools view -bS - \
 	| samtools sort - \
 	| samtools rmdup -s - - \
-	> $BAM_OUTPUT_DIR/$BAM_OUTPUT_FILE
+	> $bam_output_dir/$bam_output_file
 
-samtools index $BAM_OUTPUT_DIR/$BAM_OUTPUT_FILE
+samtools index $bam_output_dir/$bam_output_file
