b8e72d37443a231fa4526863183319c9e5b35087
Marijn van Vliet <w.m.vanvliet@gmail.com>
2021-04-01 15:02:28 +0300

    Implemented Bayesian likelihood metric

---
+++ code/statistics.R
@@ -0,0 +1,64 @@
+# Meat of the PREPRINT method
+pattern_likelihoods <- function(profiles, agg_patterns, measure = 'ML')
+{
+    distances = vector()
+    for (name in names(profiles)) {
+        sel <- colnames(profiles) == name
+        for (agg_pattern in agg_patterns) {
+            # Estimate gamma
+            if (measure == 'Bayesian') {
+                likelihood <- likelihood_Bayesian(profiles[, sel], agg_pattern[sel])
+            } else if (measure == 'ML') {
+                likelihood <- likelihood_ML(profiles[, sel], agg_pattern[sel])
+            } else {
+                stop(paste0('Invalid measure "', measure, '". Must be either "Bayesian" or "ML"'))
+            }
+
+            distances <- cbind(distances, likelihood)
+        }
+    }
+
+    colnames(distances) <- levels(interaction(names(profiles), names(agg_patterns), lex.order = TRUE))
+    profile_data(profiles) <- distances
+    profiles
+}
+
+likelihood_ML <- function(profiles, agg_pattern)
+{
+    # Formula 4 of the paper
+    alpha <- rowSums(profiles) / sum(agg_pattern)
+
+    # Formula 10 of the paper.
+    lambda <- outer(alpha, agg_pattern)
+    likelihood <- rowSums(dpois(x = profiles, lambda = lambda, log = TRUE))
+}
+
+likelihood_Bayesian <- function(profiles, agg_pattern)
+{
+    # Formula 4 of the paper
+    alpha <- rowSums(profiles) / sum(agg_pattern)
+
+    # Estimate gamma
+    gamma <- try(MASS::fitdistr(alpha[alpha > 0], 'gamma', lower = list(shape = 1E-3, rate = 1E-3), upper = list(shape = 1E3, rate = 1E3), start = list(shape=1, rate=1))$estimate)
+    if (class(gamma) == "try-error") {
+        cat('Scaling down and retrying...')
+        gamma <- MASS::fitdistr(alpha[alpha > 0] / 1000, 'gamma', lower = list(shape = 1E-3, rate = 1E-3), upper = list(shape = 1E3, rate = 1E3), start = list(shape=1, rate=1))$estimate
+        gamma[['rate']] <- gamma[['rate']] / 1000
+        cat(' success!\n')
+    }
+    a0 <- gamma[['shape']]
+    b0 <- gamma[['rate']]
+
+    # Formula 5 of the paper.
+    likelihood <- a0 * log(b0) + rowSums(profiles * log(agg_pattern)) - (a0 + rowSums(profiles)) * log(b0 + sum(agg_pattern)) + lgamma(a0 + rowSums(profiles)) - lgamma(a0) - rowSums(lgamma(profiles + 1))
+}
+
+aggregate_patterns <- function(profiles)
+{
+    profile_types <- levels(profile_type(profiles))
+    agg_patterns <- list()
+    for (typ in profile_types) {
+        agg_patterns[[typ]] <- colMeans(profiles[profile_type(profiles) == typ,])
+    }
+    agg_patterns
+}
--- code/train_predict.R
+++ code/train_predict.R
@@ -1,64 +1,39 @@
 library(caret)
+library(doParallel)
 
 distance_measure="Bayes_estimated_priors"
 path='~/scratch_cs/csb/projects/enhancer_prediction/aaltorse/Data'
 cell_line='K562'
 
 source('code/create_profiles.R')
+source('code/statistics.R')
 
 # Load the profiles
 load(paste0(path, "/", cell_line, "/data_R/profiles.RData"))
 
 # Create cross validation groups
-folds <- caret::createFolds(profile_type(profiles), k = k)
-
-# Meat of the machine learning function
-distance_to_summaries <- function(profiles, summaries, measure = 'ML')
-{
-    distances = vector()
-    for (name in names(profiles)) {
-        sel <- colnames(profiles) == name
-        for (summary in summaries) {
-            alpha <- rowSums(profiles[, sel]) / sum(summary[sel])
-            lambdas <- outer(alpha, summary[sel])
-            likelyhood <- rowSums(dpois(x = profiles[, sel], lambda = lambdas, log = TRUE))
-            distances <- cbind(distances, likelyhood)
-        }
-    }
-    colnames(distances) <- levels(interaction(names(profiles), names(summaries), lex.order = TRUE))
-    profile_data(profiles) <- distances
-    profiles
-}
-
-make_summaries <- function(profiles)
-{
-    profile_types <- levels(profile_type(profiles))
-    summaries <- list()
-    for (typ in profile_types) {
-        summaries[[typ]] <- colMeans(profiles[profile_type(profiles) == typ,])
-    }
-    summaries
-}
+folds <- caret::createFolds(profile_type(profiles), k = 5)
 
 # Evaluate performance using 5-fold cross validation, and parameter tuning
 # using an inner 5-fold cross-validation loop.
 predictions = list()
 reference = list()
 for (fold in folds) {
-    train_summaries <- make_summaries(profiles[-fold,])
-    train_data <- distance_to_summaries(profiles[-fold,], train_summaries)
-    test_data <- distance_to_summaries(profiles[fold,], train_summaries)
+    train_patterns <- aggregate_patterns(profiles[-fold,])
+    train_data <- pattern_likelihoods(profiles[-fold,], train_patterns, measure = 'Bayesian')
+    test_data <- pattern_likelihoods(profiles[fold,], train_patterns, measure = 'Bayesian')
 
     train_labels <- as.factor(ifelse(profile_type(train_data) == "enhancer",
                                      "enhancer", "not.enhancer"))
     test_labels <- as.factor(ifelse(profile_type(test_data) == "enhancer",
                                     "enhancer", "not.enhancer"))
 
-    tuneGrid <- expand.grid(C = 10 ^ seq(0, 2, length = 20), sigma = 1 / 45, Weight = 0.25)
+    tuneGrid <- expand.grid(C = 2 ^ seq(-5, 8, length = 10), sigma = 1 / 45, Weight = c(0.25, 1, 4))
     fitControl <- trainControl(method = "cv", number = 5, classProbs = TRUE, verboseIter = TRUE)
     model <- train(train_data, train_labels, method = 'svmRadialWeights',
-                   trControl = fitControl, verbose = TRUE, tuneGrid = tuneGrid)
+                   trControl = fitControl, tuneGrid = tuneGrid)
     pred <- predict(model, test_data)
+    print(model)
     cat(paste0('Accuracy: ', sum(pred == test_labels) / length(test_labels), '\n'))
 
     predictions <- unlist(list(predictions, pred))
